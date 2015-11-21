dc748a0993dde91fe0e60c4bc3585e9c7fd829ab
Sarang Dev <sarangde@timberlake.cse.buffalo.edu>
2015-11-20 19:53:07 -0500

    adding file

--- inc/Constants.h
+++ inc/Constants.h
@@ -6,9 +6,10 @@
 
 enum command_code {
 	UNKNOWN = -1, UPDATE, PACKETS, DISPLAY, DISABLE, CRASH, STEP
-};
+	};
 
-const std::string INTERFACE_NAME1="eth0";
-const std::string INTERFACE_NAME2="em2";
-const int MAX_COST=30;
-#endif
+	const std::string INTERFACE_NAME1="eth0";
+	const std::string INTERFACE_NAME2="em2";
+	const int MAX_COST=30;
+	#endif
+
